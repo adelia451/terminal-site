41d251b4cb4de41d738ee2e082ea2f40c1f88ffa
Ana <abarba1@trinity.edu>
2026-03-16 12:10:48 +0100

    error handler, fixed install | bash, fixed names

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 
   const agent = req.headers['user-agent'] || ''
 
+  //if someone tries to see in the browser
   if (!agent.includes('curl')) {
     res.send('Please view this site using:\n\ncurl ana.sh\n')
     return
@@ -19,12 +20,31 @@ app.get('/', (req, res) => {
 
   runScript('make-home', res)
 })
+
 app.get('/contacts', (req,res)=>runScript('make-contacts',res))
-app.get('/aboutme', (req,res)=>runScript('make-about',res))
+app.get('/about', (req,res)=>runScript('make-about',res))
 
 app.get('/install', (req, res) => {
   res.set('Content-Type', 'text/plain')
   res.sendFile(__dirname + '/scripts/install')
 })
+
+//app.use matches everything that didnt match above it - its the *) i used in install
+app.use((req, res) => {
+  res.set('Content-Type', 'text/plain')
+  res.status(404).send(`
+  ┌─────────────────────────────────┐
+  │  404 — page not found           │
+  └─────────────────────────────────┘
+
+  Available pages:
+
+  $ curl mimobox.sh
+  $ curl mimobox.sh/aboutme
+  $ curl mimobox.sh/contacts
+  $ curl mimobox.sh/install | bash
+
+    `)
+  })
 
 app.listen(80, () => console.log('running on port 80'))
